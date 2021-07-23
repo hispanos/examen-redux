ca6e3ff1c4415476c747787a963a0b793f6e08cd
hispanos <mailermartinez@gmail.com>
2021-07-23 13:03:16 -0500

    Render desktok and close session

--- src/containers/Dashboard.jsx
+++ src/containers/Dashboard.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom';
 
 const Dashboard = () => {
+
+    const history = useHistory();
+
+    const handleClose = () => {
+        sessionStorage.removeItem('token')
+        window.location.href = "/";
+    }
+
     return (
         <div>
-            
+            <h1>Estás en el escritorio.</h1>
+            <h2>Esta es una ruta protegida, pronto podrás hacer el CRUD aquí</h2>
+            <button onClick={handleClose}>
+                Cerrar Sesión
+            </button>
         </div>
     )
 }
